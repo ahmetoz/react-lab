e9ebad57de37dbb9ac94b1e2492188ba9e51fa73
ahmetoz <bilmuhahmet@gmail.com>
2018-04-22 15:22:07 +0300

    props

--- app/index.js
+++ app/index.js
@@ -1,15 +1,32 @@
 var React = require("react")
 var ReactDOM = require("react-dom")
 require("./index.css")
+var PropTypes = require("prop-types")
 
-class App extends React.Component {
-    render(){
+class Badge extends React.Component {
+    render() {
         return (
             <div>
-                Hello Ahmet!
+                <img
+                    src={this.props.img}
+                    alt="Avatar"
+                    style={{ width: 100, height: 100 }}
+                />
+
+                <h1>Name: {this.props.name}</h1>
+                <h3>Username: {this.props.username}</h3>
             </div>
         )
     }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'))
+Badge.propTypes = {
+    img: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    username: PropTypes.string.isRequired
+}
+
+ReactDOM.render(<Badge
+    name="Ahmet Oz"
+    username="ahmetoz"
+    img= {{ "img": "https://avatars3.githubusercontent.com/u/3469524?s=460&v=4" }} />, document.getElementById('app'))
